f54a77f938b3588968215f8db048a77a0fdc1146
Mark DeNardo <mark.denardo@gmail.com>
2019-07-28 17:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ function sayHiToGrandma(string){
     return ("YES, INDEED!");
   }
     
-  else if (string.toLowerCase === string){
+  else if (string.toLowerCase() === string){
     return ("I can't hear you!");
   }
   
-    else if ((string)===uppercase||(string)===lowercase){
-    return ("I Love you, too.");
+    else if (string === ("I love you, Grandma.")){
+    return ("I love you, too.");
   }
   
 }
